40c98528de09c058c74250b145957662042fc501
apple <apple@appledeiMac.local>
2014-07-06 10:28:31 +0000

    Initial Commit

---
+++ 点名程序/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  点名程序
+//
+//  Created by apple on 14-7-6.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
